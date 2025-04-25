1102126a57ff7d02cb9f11a4023da5daf0492fa7
nopparoot15 <99222124+nopparoot15@users.noreply.github.com>
2025-04-26 00:03:37 +0700

    Update main.py

--- main.py
+++ main.py
@@ -272,7 +272,7 @@ async def on_message(message: discord.Message):
         if notify:
             await notify.delete()
 
-        reply_content = clean_url(clean_output_text(format_response_markdown(reply)))
+        reply_content = format_response_markdown(reply)
         await smart_reply(message, reply_content)
         await store_chat(redis_instance, message.author.id, {
             "question": text,
